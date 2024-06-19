7408e16ab2d18e9cd5066b29a824016e5002c7ef
munir <smsirajulmonir@gmail.com>
2024-06-19 02:52:42 -0700

    connection with firebase build

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApyP64qC0YjQzLoAdPn5B75DpjqtQK65o",
+  authDomain: "jute-wooden.firebaseapp.com",
+  projectId: "jute-wooden",
+  storageBucket: "jute-wooden.appspot.com",
+  messagingSenderId: "466636335315",
+  appId: "1:466636335315:web:5f4931fb6709ac2b0f0c26"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
